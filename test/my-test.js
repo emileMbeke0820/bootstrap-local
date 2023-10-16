ab081e6cecddf072d2489b2d1df7989fc2d6b5fe
Emile Essam Mbeke <Emile.EssamMbeke@otto.de>
2023-10-16 22:04:35 +0200

    test

---
+++ test/my-test.js
@@ -0,0 +1,8 @@
+// Fichier de test "my-test.js"
+const assert = require("assert");
+
+describe("Exemple de test", function () {
+  it("Devrait retourner true", function () {
+    assert.strictEqual(true, true);
+  });
+});
